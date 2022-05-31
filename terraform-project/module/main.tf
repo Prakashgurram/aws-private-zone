dfaf1a19b1fe434cd59dc1a789b34045805c030c
Prakashgurram <93919118+Prakashgurram@users.noreply.github.com>
2022-05-31 12:57:35 +0530

    Update main.tf

--- terraform-project/module/main.tf
+++ terraform-project/module/main.tf
@@ -6,3 +6,31 @@ resource "aws_vpc" "vpc" {
     Name        = "custom-vpc-1"
   }
 }
+
+resource "aws_vpc" "vpc2" {
+  cidr_block           = "${var.vpc_cidr_2}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "custom-vpc-2"
+  }
+}
+
+data "aws_route53_zone" "selected" {
+  name = "cloudlab123456.info"
+  private_zone = true
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name = "www.${data.aws_route53_zone.selected.name}"
+  type = "A"
+  ttl = "300"
+  records = ["10.0.0.1"]
+}
+
+resource "aws_route53_zone_association" "vpc_one" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  vpc_id  = "${aws_vpc.vpc1.id}"
+}
+

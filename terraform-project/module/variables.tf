843fb1755950b80eb8bbcce35a4b23b2cec8569f
Prakashgurram <93919118+Prakashgurram@users.noreply.github.com>
2022-05-31 13:11:41 +0530

    Update variables.tf

--- terraform-project/module/variables.tf
+++ terraform-project/module/variables.tf
@@ -1,4 +1,10 @@
-variable "vpc_cidr" {
+variable "vpc_cidr_1" {
   description = "cidrblock for vpc-1"
-  default = "10.0.0.0/16"
+  default = "10.0.0.0/24"
 }
+
+variable "vpc_cidr_2" {
+  description = "cidrblock for vpc-2"
+  default = "10.0.1.0/24"
+}
+
